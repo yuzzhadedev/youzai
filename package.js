fb54452b5b473d3ea67a0884304cfaada245b0af
yuzzhadedev <mutustorereal@gmail.com>
2026-04-18 14:12:19 +0700

    Update package.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 {
   "name": "youz-ai",
   "version": "1.0.0",
-  "description": "Youz AI - Ai Terbaik sepanjang masa",
+  "description": "Youz AI - Ai terbaik sepanjang masa",
   "main": "server.js",
   "scripts": {
     "start": "node server.js",
@@ -9,10 +9,8 @@
   },
   "dependencies": {
     "express": "^4.18.2",
-    "cors": "^2.8.5"
-  },
-  "devDependencies": {
-    "nodemon": "^3.0.1"
+    "cors": "^2.8.5",
+    "dotenv": "^16.3.1"
   },
   "author": "Yuzz Ofc",
   "license": "MIT"
